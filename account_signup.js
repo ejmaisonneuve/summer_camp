bcbec77352dadd466024b0409add275ec9a97416
ejmaisonneuve <ejmaisonneuve@gmail.com>
2018-11-11 22:16:07 -0600

    add app and registe3r

---
+++ account_signup.js
@@ -0,0 +1,46 @@
+document.getElementById("textForm").onsubmit = validate;
+function validate () {
+
+    //set variables
+    var elt = document.getElementById("textForm");
+    var user_name = elt.userName.value;
+    var pass = elt.pass.value;
+    var rep_pass = elt.rep_pass.value;
+    var errors = "";
+
+    //test username
+    var exp1 = /[^a-zA-Z\-]/;
+    if(exp1.test(user_name)) {
+      errors += "  -Family name must contain only letters and hyphens.\n";
+    }
+
+    if(user_name[0] != user_name[0].toUpperCase()) {
+      errors += "  -Family name must start with a capital letter.\n";
+    }
+
+    //test password
+    if(pass.length > 10 || pass.length < 6) {
+      errors += "  -Password must be between 6 and 10 characters.\n";
+    }
+    exp1 = /[^a-zA-Z0-9]/;
+    if(exp1.test(pass)) {
+      errors += "  -Password must contain only numbers and letters.\n";
+    }
+    if(!(/[a-z]/.test(pass) && /[A-Z]/.test(pass) && /\d/.test(pass))) {
+      errors += "  -Password must contain at least one uppercase letter, one lowercase letter and one digit.\n";
+    }
+
+    //check repeated Password
+    if(rep_pass != pass) {
+      errors += "  -Repeat password is not the same as password.";
+    }
+
+    if(errors.length > 0) {
+      window.alert("Invalid Input: \n" + errors);
+      return false;
+    }
+
+    else {
+      return true
+    }
+}
---
+++ application.js
@@ -0,0 +1,178 @@
+function show_rows() {
+  var sib_sel = document.getElementById("num_sib");
+  var num_sibs = sib_sel.options[sib_sel.selectedIndex].value;
+  var rows_list = document.getElementsByClassName("sibling_row");
+
+  for (i = 0; i < (rows_list.length); i++) {
+    if(rows_list[i].childNodes.length > 0) {
+      rows_list[i].removeChild(rows_list[i].childNodes[0]);
+    }
+    else {
+      break;
+    }
+  }
+
+  for (i = 0; i < (num_sibs*2); i+=2) {
+    var new_input = document.createElement("input");
+    new_input.type = "text";
+    if(i == 12) {
+      new_input.placeholder = "Rest of Names";
+    }
+    else {
+      new_input.placeholder = "Name";
+    }
+    new_input.className = "sibling_info";
+    rows_list[i].appendChild(new_input);
+    new_input = document.createElement("input");
+    new_input.type = "text";
+    if(i == 12) {
+      new_input.placeholder = "Rest of Ages";
+    }
+    else {
+      new_input.placeholder = "Age";
+    }
+    new_input.className = "sibling_info";
+    rows_list[i+1].appendChild(new_input);
+  }
+}
+
+function add_pickup(item) {
+
+  var prev_name = document.getElementById("prev_name").value;
+  var prev_address = document.getElementById("prev_address").value;
+  if(prev_name.length == 0 || prev_address.length == 0) {
+    window.alert("Please fill out current person's information before adding another person.");
+    return;
+  }
+
+  var table = document.getElementById("app_fam");
+  var new_row = document.createElement("tr");
+  var new_input = create_input(item, "Name", "Edith Anne Doe", "2");
+  document.getElementById("prev_name").id = "";
+  new_input.childNodes[1].id = "prev_name";
+  new_row.appendChild(new_input);
+  new_input = create_input(item, "Relationship", "Grandmother", "1");
+  new_row.appendChild(new_input);
+  new_input = create_input(item, "Phone", "(123) 456-7890", "1");
+  new_row.appendChild(new_input);
+  table.appendChild(new_row);
+
+  var new_row = document.createElement("tr");
+  new_input = create_input(item, "Address", "1111 Left Lane, Spring, TX, 77388", "2");
+  document.getElementById("prev_address").id = "";
+  new_input.childNodes[1].id = "prev_address";
+  new_row.appendChild(new_input);
+  new_button = create_button(item);
+  new_row.append(new_button);
+  table.appendChild(new_row);
+
+  return;
+
+}
+
+function create_button(item) {
+  var new_place = document.createElement("td");
+  new_place.appendChild(item);
+  return new_place;
+
+}
+
+function create_input(item, title, ph, span) {
+  var new_place = document.createElement("td");
+  var new_title = document.createElement("div");
+  var new_text = document.createTextNode(title);
+  var new_item = document.createElement("input");
+  new_item.type = "text";
+  new_item.placeholder = ph;
+  if(span === "2") {
+    new_item.style.width = "330px";
+  }
+  new_title.appendChild(new_text);
+  new_place.appendChild(new_title);
+  new_place.colSpan = span;
+  new_place.appendChild(new_item);
+  return new_place;
+}
+
+function check_info() {
+  var problems = "";
+  problems += check_phones();
+  problems += check_parent();
+  problems += check_sibling_info();
+  problems += check_pickup();
+
+  if(problems.length > 0) {
+    window.alert(problems);
+    return false;
+  }
+  return true;
+}
+
+function check_pickup() {
+  var name = document.getElementById("prev_name").value;
+  var add = document.getElementById("prev_address").value;
+  if(name.length > 0 && add.length == 0) {
+    return "Please fill out addresses for the people who you would like to pick up your child.";
+  }
+  return "";
+}
+
+function check_parent() {
+  var dad = document.getElementById("father").value;
+  var mom = document.getElementById("mother").value;
+  if(dad.length == 0 && mom.length == 0) {
+    return "Please fill out information for at least one parent \n";
+  }
+  return "";
+}
+
+function check_phones() {
+  var phone_nums = document.getElementsByClassName("phones");
+  var count = 0;
+  var bad_nums = [];
+  var ans = "";
+
+  for(i = 0; i < phone_nums.length; i++) {
+    if(phone_nums[i].value == "") {
+      count += 1;
+    }
+  }
+  if(count == 3) {
+    return "Please fill out at least one personal phone number (home/cell) \n";
+  }
+
+
+  var exp = /\(*[0-9]{3}\)*\s*-*\s*[0-9]{3}\s*-*\s*[0-9]{4}/
+  for(i = 0; i < phone_nums.length; i++) {
+    if(!exp.test(phone_nums[i].value) && phone_nums[i].value.length > 0) {
+      bad_nums.push(phone_nums[i].id);
+    }
+  }
+  if(bad_nums.length > 0) {
+    ans = "Make sure your ";
+    for(i = 0; i <bad_nums.length; i++) {
+      if(i == bad_nums.length - 1) {
+        ans += bad_nums[i];
+      }
+      else if (i == bad_nums.length - 2) {
+        ans += bad_nums[i] + " and ";
+      }
+      else {
+        ans += bad_nums[i] + ", ";
+      }
+    }
+    ans += " phone number information is filled out correctly: (832) 123-1234 \n";
+  }
+  return ans;
+}
+
+function check_sibling_info() {
+  var sibling_info = document.getElementsByClassName("sibling_info");
+  var count = 0;
+  for(i = 0; i < sibling_info.length; i++) {
+    if(sibling_info[i].value.length == 0) {
+      return "Please fill out sibling info for each child. \n"
+    }
+  }
+  return "";
+}
